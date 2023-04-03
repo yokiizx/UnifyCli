b176f10c0097e8fdccbb7c9bf3c99166db44cdd2
yokiizx <yokiizx@163.com>
2023-04-03 16:15:06 +0800

    init idea

---
+++ bin/index.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
+const argv = yargs(hideBin(process.argv)).argv;
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+import yargs from 'yargs/yargs'
+import { hideBin } from 'yargs/helpers'
+import chalk from 'chalk'
+
+const argv = yargs(hideBin(process.argv)).argv
